7f92ad824b6698041cc5a4740b60d208e1f1799f
Vladimir Titov <vtitov4@yandex.ru>
2020-06-25 16:58:32 +0400

    Lesson-2 is done

---
+++ src/main/java/ru/titov/enterprise/servlet/Catalog.java
@@ -0,0 +1,23 @@
+package ru.titov.enterprise.servlet;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(urlPatterns = {"/catalog"})
+public class Catalog extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        RequestDispatcher dispatcher = req.getRequestDispatcher("/catalogJsp.jsp");
+        dispatcher.forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+
+    }
+}
