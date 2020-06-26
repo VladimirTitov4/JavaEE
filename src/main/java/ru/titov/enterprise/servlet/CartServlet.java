fb78c07cd44fff804ddaa2708a1260adb3ae2fd2
Vladimir Titov <vtitov4@yandex.ru>
2020-06-26 16:59:53 +0400

    minor changes if CartServlet

--- src/main/java/ru/titov/enterprise/servlet/CartServlet.java
+++ src/main/java/ru/titov/enterprise/servlet/CartServlet.java
@@ -9,7 +9,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 @WebServlet(name = "cart", urlPatterns = {"/cart"})
-public class Cart extends HttpServlet {
+public class CartServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
         RequestDispatcher dispatcher = req.getRequestDispatcher("/cartJsp.jsp");
